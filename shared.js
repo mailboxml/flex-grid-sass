7d59b8d3f7d24176f2d25c3c66c3f8390b5677a4
MB <93428372+mailboxml@users.noreply.github.com>
2022-10-07 14:43:59 +0300

    hamburger menu java script

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(selectPlanButtons);
 
@@ -15,13 +17,27 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 }
 
 
-backdrop.addEventListener("click", functie_inchidere);
-modalNoButton.addEventListener("click", functie_inchidere);
+backdrop.addEventListener("click", function() {
 
-function functie_inchidere() {
+    mobileNav.style.display = "none";
+    functie_inchidere_modal();
+}
+);     // modal si backdrop sunt inchise default.
+
+
+modalNoButton.addEventListener("click", functie_inchidere_modal);
+
+function functie_inchidere_modal() {
     backdrop.style.display = "none";
     modal.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function() {
+ 
+    mobileNav.style.display = "block";
+    backdrop.style.display = "block"
+}
+);
 
 
 // backdrop.addEventListener('click', function() {
